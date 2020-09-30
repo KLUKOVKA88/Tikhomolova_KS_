751cb4cbfe03cf0c8504526b679d3e2aa921ecf0
Klukovka88 <8_klukovka_8@inbox.ru>
2020-09-30 20:14:49 +0300

    Копирование информации

---
+++ ConsoleTests/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace ConsoleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var to = new MailAddress("k3incho@inbox.ru", "Костя");           //кому отправляем
+            var from = new MailAddress("8_klukovka_8@inbox.ru", "Костя");    //от кого отправляем
+
+            var message = new MailMessage(from, to);    //создаем почтовое отправление
+            //var msg = new MailAddress("user@server.ru", "qwe@ASD.ru");
+
+            message.Subject = "Заголовок письма от " + DateTime.Now;
+            message.Body = "Тело текстового письма + " + DateTime.Now;
+
+            //создаем клиента SMTP почты, через который будет отправляться почта
+            var client = new SmtpClient("smtp.mail.ru"/*, "smtp.yandex.ru", 587 */);
+            client.EnableSsl = true;
+
+            //указываем учетные данные почты клиента
+            client.Credentials = new NetworkCredential
+            {
+                UserName = "user_name",
+                Password = "PassWord!"
+            };
+
+            //отправляем сообщение
+            client.Send(message);
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
