6ef2c608e79662d9b238d2950cb94dab3bc8c4dd
Klukovka88 <8_klukovka_8@inbox.ru>
2020-10-01 14:02:01 +0300

    Управление таймером по средством CheckBox'а

--- WPFTests/ViewModels/MainWindowViewModel.cs
+++ WPFTests/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using WPFTests.ViewModels.Base;
+﻿using System;
+using System.Timers;
+using WPFTests.ViewModels.Base;
 
 namespace WPFTests.ViewModels
 {
@@ -8,14 +10,46 @@ namespace WPFTests.ViewModels
 
         public string Title
         {
-
             get => _Title;
+            set => Set(ref _Title, value);
+
+
+            //{
+            //    if (_Title == value) return;
+            //    _Title = value;
+            //    //OnPropertyChanged("Title");
+            //    OnPropertyChanged();
+            //}
+        }
+
+        
+        public DateTime CurrentTime => DateTime.Now;
+
+        private bool _TimerEnabled = true;
+
+        public bool TimerEnabled
+        {
+            get => _TimerEnabled;
             set
             {
-                if (_Title == value) return;
-                _Title = value;
-                OnPropertyChanged("Title");
+                if (!Set(ref _TimerEnabled, value)) return;
+                _Timer.Enabled = value;
             }
+        }
+
+        private readonly Timer _Timer;
+
+        public MainWindowViewModel()
+        {
+            _Timer = new Timer(100);
+            _Timer.Elapsed += OnTimerElapsed;
+            _Timer.AutoReset = true;
+            _Timer.Enabled = true;
+        }
+
+        private void OnTimerElapsed(object Sender, ElapsedEventArgs E)
+        {
+            OnPropertyChanged(nameof(CurrentTime));
         }
     }
 }
