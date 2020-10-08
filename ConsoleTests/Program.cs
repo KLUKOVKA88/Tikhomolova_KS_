89e0149a9de2e4474a969f78ce7280c8a6d6750f
Klukovka88 <8_klukovka_8@inbox.ru>
2020-10-08 20:39:48 +0300

    Подготовка

--- ConsoleTests/Program.cs
+++ ConsoleTests/Program.cs
@@ -1,8 +1,5 @@
-﻿using MailSender.lib.Interfaces;
-using MailSender.lib.Service;
-using System;
-using System.Net;
-using System.Net.Mail;
+﻿using System;
+
 
 namespace ConsoleTests
 {
@@ -10,14 +7,8 @@ namespace ConsoleTests
     {
         static void Main(string[] args)
         {
-            IEncryptorService cryptor = new Rfc2898Encryptor();
-
-            var str = "Hello World!";
-            const string password = "MailSender!";
-
-            var crypted_str = cryptor.Encrypt(str, password);
-
-            var decrypted_str = cryptor.Decrypt(crypted_str, password);
+            Console.WriteLine("Главный поток работу закончил");
+            Console.ReadLine();
            
         }
     }
