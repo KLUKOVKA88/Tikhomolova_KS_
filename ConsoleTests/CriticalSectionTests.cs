6335796853d98a67baabaf70786aec019aea063e
Klukovka88 <8_klukovka_8@inbox.ru>
2020-10-09 11:03:56 +0300

    Синхронизация потоков с помощью lock, AutoResetEvent, ManualResetEvent

--- ConsoleTests/CriticalSectionTests.cs
+++ ConsoleTests/CriticalSectionTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.IO;
 
 namespace ConsoleTests
 {
@@ -9,7 +11,103 @@ namespace ConsoleTests
 
         public static void Start() 
         {
-        
+            //LockSynchronizationTest();
+
+            var manual_reset_event = new ManualResetEvent(false);
+            var auto_reset_event = new AutoResetEvent(false);
+
+            EventWaitHandle starter = manual_reset_event;
+
+            for(var i =0; i < 10; i++)
+            {
+                var local_i = i;
+                new Thread(() =>
+                {
+                    Console.WriteLine("Поток {0} запущен", local_i);
+                    starter.WaitOne();
+                    starter.Reset();
+                    Console.WriteLine("Поток {0} завершил свою работу", local_i);                    
+                }).Start();
+            }
+
+            Console.WriteLine("Все потоки созданы и готовы к работе.");
+            Console.ReadLine();
+
+            starter.Set();
+
+            Console.ReadLine();
+
+            starter.Set();
+
+            Console.ReadLine();
+
+            starter.Set();
+
+            Console.ReadLine();
+
+            starter.Set();
+
+            Console.ReadLine();
+
+        }
+
+        private static void LockSynchronizationTest()
+        {
+            var threads = new Thread[10];
+
+            for (var i = 0; i < threads.Length; i++)
+            {
+                var local_i = i;
+                threads[i] = new Thread(() => PrintData($"Message from thread {local_i}", 10));
+            }
+
+            for (var i = 0; i < threads.Length; i++)
+                threads[i].Start();
+        }
+
+        private static readonly object __SyncRoot = new object();
+
+
+        private static void PrintData(string Message, int Count)
+        {
+            for(var i =0; i < Count; i++)
+            {
+                lock(__SyncRoot)
+                {
+                    Console.Write("Thread id:{0}", Thread.CurrentThread.ManagedThreadId);
+                    Console.Write("\t");
+                    Console.Write(Message);
+                    Console.WriteLine();
+                }
+
+                //Monitor.Enter(__SyncRoot);
+                //try 
+                //{
+                //    Console.Write("Thread id:{0}", Thread.CurrentThread.ManagedThreadId);
+                //    Console.Write("\t");
+                //    Console.Write(Message);
+                //    Console.WriteLine();
+                //}
+                //finally
+                //{
+                //    Monitor.Exit(__SyncRoot);
+                //}
+            }
+        }
+    }
+
+    //[Synchronization] Для NET.Framework
+    public class FileLogger : ContextBoundObject
+    {
+        private readonly string _LogFileName;
+        public FileLogger(string LogFileName)
+        {
+            _LogFileName = LogFileName;
+        }
+
+        public void Log(string Message)
+        {
+            File.WriteAllText(_LogFileName, Message);
         }
     }
 }
