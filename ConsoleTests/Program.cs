686fccc92786857041f10978952c7a0bb275dd3f
Klukovka88 <8_klukovka_8@inbox.ru>
2020-10-09 12:04:31 +0300

    Пул потоков

--- ConsoleTests/Program.cs
+++ ConsoleTests/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleTests
         static void Main(string[] args)
         {
             //ThreadTests.Start();
-            CriticalSectionTests.Start();
+            ThreadPoolTests.Start();
 
             Console.WriteLine("Главный поток работу закончил");
             Console.ReadLine();
