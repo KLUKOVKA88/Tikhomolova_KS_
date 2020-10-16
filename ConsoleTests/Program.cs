6a5ea65cd9a8f188dfe5b8fdc63fdf4c5cfd997d
Klukovka88 <8_klukovka_8@inbox.ru>
2020-10-16 10:28:16 +0300

    Простейшая БД

--- ConsoleTests/Program.cs
+++ ConsoleTests/Program.cs
@@ -1,20 +1,70 @@
-﻿using System;
+﻿using ConsoleTests.Data.Entityes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 
 namespace ConsoleTests
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            //TPLOverview.Start();
-            var task = AsyncAwayTest.StartAsync();
-            var process_messages_task = AsyncAwayTest.ProcessDataTestAsync();
+        static async Task Main(string[] args)
+        {       
+            const string connection_str = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = Students.DB; Integrated Security = True";
 
-            Console.WriteLine("Тестовая задача заупщена и мы ее ждем!...");
+            //var service_collection = new ServiceCollection();
+            //service_collection.AddDbContext<StudentsDB>(opt => opt.UseSqlServer(connection_str));
 
-            //task.Wait();
-            Task.WaitAll(task, process_messages_task);
+            //var services = service_collection.BuildServiceProvider();
+
+            //using (var db = services.GetRequiredService<StudentsDB>())
+            //{
+
+            //}
+
+            using (var db = new StudentsDB(new DbContextOptionsBuilder<StudentsDB>().UseSqlServer(connection_str).Options))
+            {
+                await db.Database.EnsureCreatedAsync();
+
+                var students_count = await db.Students.CountAsync();
+
+                Console.WriteLine("Число студентов в БД = {0}", students_count);
+            }
+
+            using (var db = new StudentsDB(new DbContextOptionsBuilder<StudentsDB>().UseSqlServer(connection_str).Options))
+            {
+
+                var k = 0;
+                if (await db.Students.CountAsync() == 0)
+                {
+                    for (var i = 0; i < 10; i++)
+                    {
+                        var group = new Group
+                        {
+                            Name = $"Группа {i}",
+                            Description = $"Описание группы {i}",
+                            Students = new List<Student>()
+                        };
+
+                        for (var J = 0; J < 10; J++)
+                        {
+                            var student = new Student
+                            {
+                                Name = $"Студент {k}",
+                                Surname = $"Surname {k}",
+                                Patronymic = $"Patronymic {k}"
+                            };
+                            k++;
+                            group.Students.Add(student);
+                        }
+
+                        await db.Groups.AddAsync(group);
+                    }
+                    await db.SaveChangesAsync();
+                }             
+            }
 
             Console.WriteLine("Главный поток работу закончил!");
             Console.ReadLine();
